8ff935994b58162df28bd767670c4043f5cb8913
Eun Chong Ju <vevsing@gmail.com>
2020-09-10 16:43:34 +0900

    Update view.js

--- js/view.js
+++ js/view.js
@@ -3,9 +3,36 @@
 // 이 파일은 보여주기 위한 것이다.
 
 var View = function() {
+	this.new = function(w, h) {
+		var code = start(w,h);
+		document.getElementById('hexcell-map').innerHTML = code;
+	};
 	
+	function start(w,h) {
+		var code = '';
+		for (var i = 0; i < w; i++) {
+			var maxH = (i%2==0) ? (h-1) : h;
+			code += '<div class="hexcell-line">';
+			for (var j = 0; j < maxH; j++) {
+				code += '<button class="hexcell" id="c-'+i+'-'+j+'"></button>';
+			}
+			code += '</div>';
+		}
+		return code;
+	}
+	
+	this.update = function(data) {
+		
+	};
+	
+	this.end = function() {
+		
+	};
 };
 
-var sample = new View();
+//// View.js의 사용 예:
+
+// view를 생성한다.
+var view = new View();
 
 
