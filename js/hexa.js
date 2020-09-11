50e7177b312040a528a9f266d4c5415a85e48b83
Eun Chong Ju <vevsing@gmail.com>
2020-09-11 13:04:11 +0900

    Update it that like framework
    update script

--- js/hexa.js
+++ js/hexa.js
@@ -1,32 +1,275 @@
 
-function setMapDisplay(w, h) {
-	var code = getCodeMapDisplay(w,h);
-	document.getElementById('hexcell-map').innerHTML = code;
-}
-
-// 파라미터의 h는 maxHeight임.
-function getCodeMapDisplay(w, h) {
-	var code = '';
-	var width = w;
-	var index = 1;
+var Hexagonsweeper = function() {
+	//// 모델 영역에서의 코드
+	// 맵 데이터
+	var map = null;
+	// 맵 기본 설정
+	var mapWidth = 0;	// 위아래로 돌출된 육각형이 모여 이루는 맵이므로 width는 그대로,
+	var mapMaxHeight = 0;	// height는 최대 높이라 하며, 최소 높이는 height-1과 같다.
+	var mapMinHeight = 0;
+	var mapMineNumber = 0;
 	
-	for (var i = 0; i < width; i++) {
-		var height = (i%2==0) ? (h-1) : h;
+	// 생성자 호출 후 먼저 실행되는 함수
+	this.start = function(width, height, number) {
+		// start()의 설정에 오류가 있으면 생성을 거부한다.
+		if (!this.check(width, height, number)) return 'ERROR';
 		
-		code += '<div class="hexcell-line">';
+		// 맵의 속성 기본 값부터 설정한다.
+		mapWidth = width;
+		mapMaxHeight = height;
+		mapMinHeight = height - 1;
+		mapMineNumber = number;
 		
-		for (var j = 0; j < height; j++) {
-			code += '<button class="hexcell">' + (index++) + '</button>';
-		}
-		
-		code += '</div>';
+		// 맵을 생성한다.
+		setMap();
+	};
+	
+	this.check = function(width, height, number) {	// height는 maxHeight임
+		return getMaxNumber(width, height) >= number;	// 최대 갯수가 number보다 크지 않거나 같으면 true 반환
+	};
+	// 맵에서 지뢰를 최대(지뢰배치금지구역 제외)한 넣을 수 있는 수를 구한다. (19개는 지뢰배치금지구역의 지뢰 수)
+	function getMaxNumber(w, h) {
+		var maxSize = w*(h-1);
+		maxSize += ((w%2==0)?w:w-1)/2;
+//		return ((w*(h-1)+((w%2==0) ? w : w-1))/2-19);
+		return maxSize-19;
 	}
 	
-	return code;
-}
-
-//setMapDisplay(10,10);
-
-
-
+	// 해당 아이템 값을 좌표로 조회
+	function getItem(x, y) {	// 아마 저거를 프라이빗으로 처리해야 하니 this를 function으로 바꿀 것이다.
+		return (isIndexOutOfMap(x,y) ? map[x][y] : undefined);
+	}
+	
+	// 좌표로 아이템 값 배치
+	function setItem(x, y, val) {
+		map[x][y] = val;
+	}
+	
+	// 맵 조회
+	function getMap() {
+		return map;
+	}
+	// TEMP : this.getMap()
+	this.getMap = function() {
+		return getMap();
+	};
+	
+	// 맵을 세팅
+	function setMap() {
+		var tmpMap = new Array(mapWidth);
+		
+		for (var i = 0; i < tmpMap.length; i++) {
+			tmpMap[i] = new Array(((i%2 == 0) ? mapMinHeight : mapMaxHeight));
+			
+			for (var j = 0; j < tmpMap[i].length; j++) {
+				tmpMap[i][j] = 0;
+			}
+		}
+		
+		map = tmpMap;
+	} 
+	
+	// 처음으로 셀을 눌렀을 때 호출
+	this.landStart = function(x, y) {
+		this.setNoPlacementZone(x,y);
+		this.setLandMine();
+		this.unsetNoPlacementZone(x,y);
+		return true;
+	};
+	
+	// 처음 클릭한 곳의 주변을 지뢰 배치 금지 구역으로 설정 : 원점부터 주변 2칸까지
+	this.setNoPlacementZone = function(x, y) {
+		var a = (x%2==0) ? -1 : -2;
+		
+		for (var i = -2; i <= 2; i++) {		// i : -2, -1, 0, +1, +2
+			var b = Math.abs(i);
+			
+			if (b == 1) {
+				for (var j = 0; j < 4; j++) setNoTouchZone(x+i, y+a+j);
+			}
+			else if (b == 2) {
+				for (var k = -1; k <= 1; k++) setNoTouchZone(x+i, y+k);
+			}
+			setNoTouchZone(x, y+i);	// 터치하는 부분을 포함
+		}
+	}
+	
+	function setNoTouchZone(x, y) {
+		if (validIndexForLandMines(x,y)) setItem(x, y, -2);	// 지뢰는 -1, 지뢰배치 금지는 -2
+	}
+	
+	// 지뢰를 배치 (지뢰를 전부 배치할 때까지 반복 : 지뢰 배치 후 주변 값 증산)
+	this.setLandMine = function() {
+		var num = mapMineNumber;
+		var count = 0;
+		
+		// 지뢰를 전부 배치할 때까지 반복
+		while (count < num) {
+			// 배치할 좌표를 랜덤으로 생성한다.
+			var rand = randomSetMine();
+			
+			// 생성한 좌표에 이미 지뢰가 있거나 배치 금지구역인지 확인한다.
+			if (validIndexForLandMines(rand.x, rand.y)) {
+				// 지뢰나 노터치존이 아니므로 배치한다.
+				setItem(rand.x, rand.y, -1);
+				
+				// 배치한 자리의 주변을 증산한다.
+				setUpAround(rand.x, rand.y);
+				
+				// 카운트를 증산한다.
+				count++;
+			}
+		}
+		console.log('Successful Set up Land Mines!');
+	}
+	// 지뢰가 있는 곳의 주변 값을 하나씩 올린다.
+	function setUpAround(x, y) {
+		var a = ((x%2==0) ? 0 : -1);
+		
+		setUpItem(x, y-1);
+		setUpItem(x, y+1);
+		
+		for (var i = -1; i <= 1; i+=2) {
+			for (var j = a; j <= (a+1); j++) {
+				setUpItem(x+i, y+j);
+			}
+		}
+	}
+	
+	// 지뢰가 있는 곳 주변을 하나씩 증산시키는데, 지뢰가 없을 때만 적용된다.
+	function setUpItem(x, y) {
+		if (validIndexForLandMines(x,y)) setItem(x, y, getItem(x,y)+1);
+	}
+	
+	// 지뢰 배치 금지 구역 해제
+	this.unsetNoPlacementZone = function(x, y) {
+		var a = (x%2==0) ? -1 : -2;
+		
+		for (var i = -2; i <= 2; i++) {		// i : -2, -1, 0, +1, +2
+			var b = Math.abs(i);
+			
+			if (b == 1) {
+				for (var j = 0; j < 4; j++) unsetNoTouchZone(x+i, y+a+j);
+			}
+			else if (b == 2) {
+				for (var k = -1; k <= 1; k++) unsetNoTouchZone(x+i, y+k);
+			}
+			if (i!=0) unsetNoTouchZone(x, y+i);
+		}
+	}
+	// 지뢰배치금지구역 해제 단계에서 지뢰가 있거나 금지구역이 아닌 경우, 주변 값이 설정된 경우 false를 반환
+	function unsetNoTouchZone(x, y) {
+		var item = getItem(x,y);
+		if (item == -2 || (!(item >= 1 || item == undefined))) setItem(x,y,0);
+	}
+	
+	// 지뢰 배치에 있어서 인덱스를 랜덤으로 추천하는 함수
+	/*this.randomSetMine = */
+	function randomSetMine() {
+		var width = mapWidth;
+		var height = mapMaxHeight;
+		
+		// randX에 따라 randY의 가능 난수의 경우의 수가 달라지므로
+		// randX부터 생성한 다음, 이 수에 따라 randY를 생성한다.
+		var randX = Math.floor(Math.random() * width);
+		var randY = Math.floor(Math.random() * ((randX%2==0) ? (height-1) : height));
+		// x가 0, 2 같은 짝수일 때 -> 최소 높이값으로 설정
+		
+		return {
+			x: randX,
+			y: randY
+		};
+	}
+	
+	// 지뢰 배치에 있어서 유효한 인덱스인지 확인 (밖으로 벗어나거나 지뢰가 있거나 배치금지구역에 있으면 false를 반환)
+	/*this.validIndexForLandMines = */
+	function validIndexForLandMines(x, y) {
+		var item = getItem(x, y);
+		return !(item == -1 || item == -2 || item == undefined);
+	}
+	
+	/*
+	// 주변을 탐색 또는 배치를 관여하는 함수. 일단은 샘플 형태로 짜놓음. 함수형 프로그래밍을 참조하여 개조할 예정
+	this.around = function(x, y) {
+		var a = ((x%2==0) ? 0 : -1);
+		
+		validIndexForLandMines(x, y-1);
+		validIndexForLandMines(x, y+1);
+		
+		for (var i = -1; i <= 1; i+=2) {
+			for (var j = a; j <= (a+1); j++) {
+				validIndexForLandMines(x+i, y+j);
+			}
+		}
+	}
+	*/
+	
+	// 인덱스 값 유효 확인 (밖으로 벗어나면 false 반환)
+	function isIndexOutOfMap(x, y) {
+		var maxWidth = mapWidth;
+		var maxHeight = ((x%2==0) ? mapMinHeight : mapMaxHeight);
+		
+		return (((x < 0) || (x >= maxWidth)) || ((y < 0) || (y >= maxHeight))) ? false : true;
+	}
+	
+	// 여기서 하나 해결해야 할 점이 하나 있다.
+	// 클릭했을 때, 0이면 주변 값을 걷어내어 1 이상이 나올 때까지 반복한다.
+	// 걷어내고 0의 지대가 없을 때까지 걷어내어 보여주는 것을 어떻게 처리할 것인가?
+	
+	//// 컨트롤러 코드
+	
+	// 처음 클릭할 때 호출하는 함수
+	this.firstClick = function(x,y) {
+//		if (isIndexOutOfMap(x,y)) return this.landStart(x,y);	// 인덱스가 유효할 때만 실행
+//		else return false;
+		return (isIndexOutOfMap(x,y) ? this.landStart(x,y) : false);
+	};
+	
+	// 두번째 이상부터 클릭할 때 호출하는 함수
+	this.click = function(x,y) {
+		return (
+			(isIndexOutOfMap(x,y)) ? (
+				(getItem(x,y)==-1) ? false : true) : undefined);
+	};	// 맵에 벗어나면 undefined, 벗어나지 않더라도 지뢰가 있으면 false, 없으면 true를 반환한다.
+	
+	
+	//// 모델뷰(또는 뷰)의 코드
+	
+	this.new = function(w, h) {
+		var code = start(w,h);
+		document.getElementById('hexcell-map').innerHTML = code;
+	};
+	
+	function start(w,h) {
+		var code = '';
+		for (var i = 0; i < w; i++) {
+			var maxH = (i%2==0) ? (h-1) : h;
+			code += '<div class="hexcell-line">';
+			for (var j = 0; j < maxH; j++) {
+				code += '<button class="hexcell" id="c-'+i+'-'+j+'"></button>';
+			}
+			code += '</div>';
+		}
+		return code;
+	}
+	
+	this.update = function(data) {
+		
+	};
+	
+	this.updateCell = function(x, y, data) {
+		
+	};
+	
+	this.end = function() {
+		
+	};
+	
+	this.gameStart = function() {
+		
+	}
+	this.gameOver = function() {
+		
+	}
+};
 
---
+++ js/script.js
@@ -0,0 +1,87 @@
+
+
+// 맵의 설정(시작화면)의 모든 함수
+function setLevel(level) {
+	switch(level) {
+		case 1:
+			setSize(5,5,3);
+			break;
+		case 2:
+			setSize(7,9,7);
+			break;
+		case 3:
+			setSize(15,15,10);
+			break;
+	}
+}
+function setSize(width, height, number) {
+	setWidth(width);
+	setMaxHeight(height);
+	setMinHeight(height);
+	setNumber(number);
+}
+function changeHeight() {
+	setMinHeight(getHeight());
+}
+function getWidth() { return getValueOfId('width'); }
+function getHeight() { return getValueOfId('maxHeight'); }
+function getNumber() { return getValueOfId('number'); }
+function setWidth(width) { setValueOfId('width', width); }
+function setMaxHeight(height) { setValueOfId('maxHeight', height); }
+function setMinHeight(height) { changeValueOfId('minHeight', height-1); }
+function setNumber(number) { setValueOfId('number', number); }
+function resetLevel() { setSize(10,10,1); }
+
+// 기본 설정
+var hexa = null;
+
+// 처음 화면이 표시될 때 호출되는 것
+function start() {
+	hexa = new Hexagonsweeper();
+}
+
+// 게임 시작을 누르면 맵을 생성하기 위해 생성자를 호출하는 함수
+function gameStart() {
+	hexa.start();
+}
+function getMapOptions() {
+	var width = getWidth();
+	var height = getHeight();
+	var number = getNumber();
+	
+	return {
+		width: width,
+		height: height,
+		number: number
+	};
+}
+
+
+// 게임 시작 버튼을 누른 후 맵에 표시되는 일을 함
+
+
+
+// 시작 후 셀을 누르는 이벤트를 감지하고 처리
+
+
+
+// 셀 누르는 것에 따라 게임 오버가 되는지, 계속 진행되는지에 따라 달라진다.
+
+
+// 게임 오버가 아닌 계속 진행형일 경우를 처리
+
+
+// 게임 오버로 나올 경우, 게임오버로 처리하기 위해 처리
+
+
+
+// 어떤 아이디로부터 값을 얻어내는 작업을 함수화하여 간략하게 줄임
+function getValueOfId(id) {
+	return document.getElementById(id).value;
+}
+function setValueOfId(id, value) {
+	document.getElementById(id).value = value;
+}
+function changeValueOfId(id, value) {
+	document.getElementById(id).innerHTML = value;
+}
